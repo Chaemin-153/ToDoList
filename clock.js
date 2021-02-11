1203419dfef217b8fff4e20f3a8641b230c6182c
Chaemin-153 <0520032@naver.com>
2021-02-11 22:11:34 +0900

    +blinking

--- clock.js
+++ clock.js
@@ -1,6 +1,5 @@
 const clockContainer = document.querySelector(".js-clock"),
     clockTitle = clockContainer.querySelector("h1");
-
 function getTime(){
     const date = new Date();
     const min = date.getMinutes();
@@ -12,8 +11,20 @@ function getTime(){
     }`;
 }
 
+function blink(){
+    const date = new Date();
+    const min = date.getMinutes();
+    const hours = date.getHours();
+    const sec = date.getSeconds();
+    clockTitle.innerText = `${
+        hours < 10 ? `0${hours}` : hours 
+    } ${min < 10 ? `0${min}` : min
+    }`;
+}
+
 function init(){
     getTime();
+    setInterval(blink, 500);
     setInterval(getTime, 1000);
 }
 
